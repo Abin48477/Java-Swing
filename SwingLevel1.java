697518093f469ad6225dffc7c4383498c817feff
Abin Ghimire <ghimireabin56@gmail.com>
2025-06-17 15:57:48 +0545

    updated

---
+++ SwingLevel1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+    public class SwingLevel1 {
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("Hare Krishna");
+            frame.setSize(300, 200);
+            frame.setDefaultCloseOperation(3);
+            frame.setVisible(true);
+        }
+    }
+
+
